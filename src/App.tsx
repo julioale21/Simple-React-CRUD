b4b492d0f4aec8180d09d33a3289e1b3bed8bdd6
Julio Romero <julio_ale21@hotmail.com>
2021-04-10 19:13:10 -0300

    chore: initial commit and added eslint + prettier

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CrudApp from "./components/CrudApp";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>React Exercises</h1>
+      <CrudApp />
+    </>
+  );
+};
+
+export default App;
