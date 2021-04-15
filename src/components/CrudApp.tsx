9fd0f2f5d12c5d79114118b4a03455a9ca9a508b
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-04-15 12:28:26 -0300

    chore: added custom fetch helper

--- src/components/CrudApp.tsx
+++ src/components/CrudApp.tsx
@@ -3,36 +3,8 @@ import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
 import { Knight } from "./types";
 
-const initialDb: Knight[] = [
-  {
-    id: 1,
-    name: "Seiya",
-    constellation: "Pegaso",
-  },
-  {
-    id: 2,
-    name: "Shiryu",
-    constellation: "Dragon",
-  },
-  {
-    id: 3,
-    name: "Hyoga",
-    constellation: "Cisne",
-  },
-  {
-    id: 4,
-    name: "Shun",
-    constellation: "Andromeda",
-  },
-  {
-    id: 5,
-    name: "Ikki",
-    constellation: "Fenix",
-  },
-];
-
 const CrudApp: React.FC = () => {
-  const [db, setDb] = useState<Knight[]>(initialDb);
+  const [db, setDb] = useState<Knight[]>([]);
   const [dataToEdit, setDataToEdit] = useState({} as Knight);
 
   const create = (data: Knight) => {
